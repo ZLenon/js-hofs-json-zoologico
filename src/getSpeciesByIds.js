ec0176adf715b3821ef0abe57f963f883d953950
ZLenon <lenoninformatica@gmail.com>
2022-08-26 16:30:22 -0300

    Requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  return species.filter((array) => ids.includes(array.id));
+
 }
-1131321
+//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
