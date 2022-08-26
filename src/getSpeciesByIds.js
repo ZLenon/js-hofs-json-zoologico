4af65227d8067c425cd7024073b64dbda4112b5d
ZLenon <lenoninformatica@gmail.com>
2022-08-26 18:38:35 -0300

    Erros de Lint 📖 resolvidos

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,7 +4,6 @@ const { species } = data;
 
 function getSpeciesByIds(...ids) {
   return species.filter((array) => ids.includes(array.id));
-
 }
-//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
