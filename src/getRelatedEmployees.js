9fa10b03766c2111b7715c99582041dcf7b0156e
lenon nascimento <lenoninformatica@gmail.com>
2022-08-27 20:06:58 -0300

    Requisito 4 feito novamente tinha me esquecido de um detalhe

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,15 +3,15 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function isManager(id) {
-  return employees.some((numid) => numid.id.includes(id));
+  return employees.some((verificaid) => verificaid.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    return employees.filter((gerente) => gerente.managers.includes(managerId))
-      .map((nomes) => `${nomes.firstName} ${nomes.lastName}`);
+    return employees.filter((gerentes) => gerentes.managers.includes(managerId))
+      .map((nome) => `${nome.firstName} ${nome.lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 module.exports = { isManager, getRelatedEmployees };
