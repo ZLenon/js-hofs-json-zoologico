283fbc778694b73e12fd8dec771411b0cbec762e
lenon nascimento <lenoninformatica@gmail.com>
2022-08-28 20:16:24 -0300

    Requisito 8 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants
+    .filter((crianca) => crianca.age < 18).length;
+  const adult = entrants
+    .filter((adulto) => adulto.age >= 18 && adulto.age < 50).length;
+  const senior = entrants
+    .filter((idoso) => idoso.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  if (!Array.isArray(entrants) && typeof entrants === 'object' && Object
+    .keys(entrants).length === 0) {
+    return 0;
+  }
+  const verifyAge = countEntrants(entrants);
+  const valueChild = prices.child * verifyAge.child;
+  const valueAdult = prices.adult * verifyAge.adult;
+  const valueSenior = prices.senior * verifyAge.senior;
+  return valueAdult + valueChild + valueSenior;
 }
-
+// console.log(countEntrants(['entrants']));
 module.exports = { calculateEntry, countEntrants };
