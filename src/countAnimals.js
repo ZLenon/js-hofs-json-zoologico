18f2a75a5e066428f9937cccc5996c3d229ae89d
lenon nascimento <lenoninformatica@gmail.com>
2022-08-27 21:36:31 -0300

    Requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.sex) {
+    return species.find((specie) => specie.name === animal.specie).residents
+      .filter((genero) => genero.sex === animal.sex).length;
+  }
+  if (animal.specie) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+}
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'giraffes' }));
+// console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
