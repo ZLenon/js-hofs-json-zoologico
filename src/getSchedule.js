57f640268e04fcd9dc5b29671e16dab9dadd5644
lenon nascimento <lenoninformatica@gmail.com>
2022-08-29 16:46:19 -0300

    Requisito 9 feito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const { hours, species } = data;
 
+const days = Object.keys(hours);
+
+const alldays = () => {
+  return days.reduce((acc, curr) => {
+    acc[curr] = { officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm` };
+    acc[curr].exhibition = species.filter((param) => param.availability.includes(curr))
+      .map((nome) => nome.name);
+    if (curr === 'Monday') {
+      acc[curr].officeHour = 'CLOSED';
+      acc[curr].exhibition = 'The zoo will be closed!';
+    }
+    return acc;
+  }, {});
+};
+
+const oneday = (scheduleTarget) => {
+  const days = {
+    officeHour: `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`
+  };
+  days.exhibition = species.filter((param) => param.availability
+    .includes(scheduleTarget)).map((param) => param.name);
+  if (scheduleTarget === 'Monday') {
+    days.officeHour = 'CLOSED';
+    days.exhibition = 'The zoo will be closed!';
+  }
+  return { [scheduleTarget]: days };
+};
+
+function getSchedule(scheduleTarget) {
+  if (!scheduleTarget) {
+    return alldays();
+  }
+  if (days.includes(scheduleTarget)) {
+    return oneday(scheduleTarget);
+  }
+  if (species.some((nome) => nome.name === scheduleTarget)) {
+    return species.find((name) => name.name.includes(scheduleTarget)).availability;
+  }
+  return alldays();
+}
+// console.log(getSchedule('lions'));
 module.exports = getSchedule;
