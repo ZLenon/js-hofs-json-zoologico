c457d8bdca0c93ae34bed2ec35d2dc9138745383
lenon nascimento <lenoninformatica@gmail.com>
2022-08-29 21:01:52 -0300

    Requisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { species, employees } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const firstAnimal = employees.find((people) => people.id === id).responsibleFor[0];
+  const animalOld = species.find((animalId) => animalId.id.includes(firstAnimal)).residents
+    .sort((a, b) => b.age - a.age)[0];
+  const infoAnimalOld = Object.values(animalOld);
+  return infoAnimalOld;
+}
+// console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
