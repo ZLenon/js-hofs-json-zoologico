1fa731167eca1e5651134ea8441d072c89650802
lenon nascimento <lenoninformatica@gmail.com>
2022-09-05 19:22:15 -0300

    Requisito 11 feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
 
+const oneEmployees = (param) => employees.filter((l) => l.firstName === param.name
+  || l.lastName === param.name || l.id === param.id)
+  .reduce((acc, curr) => {
+    const oneRegister = acc;
+    acc.id = curr.id;
+    acc.fullName = `${curr.firstName} ${curr.lastName}`;
+    acc.species = species.filter((animals) => curr.responsibleFor.includes(animals.id))
+      .map((nome) => nome.name);
+    acc.locations = species.filter((animal) => curr.responsibleFor.includes(animal.id))
+      .map((coordinate) => coordinate.location);
+
+    return oneRegister;
+  }, {});
+
+const allEmployees = () => employees.map((array) => {
+  const allInfoEmployees = {
+    id: array.id,
+    fullName: `${array.firstName} ${array.lastName}`,
+    species: species.filter((param) => array.responsibleFor.includes(param.id))
+      .map((nome) => nome.name),
+    locations: species.filter((param) => array.responsibleFor.includes(param.id))
+      .map((local) => local.location),
+  };
+  return allInfoEmployees;
+}, []);
+
+function getEmployeesCoverage(param) {
+  if (!param) {
+    return allEmployees();
+  }
+  if (employees.some((check) => check.firstName === param.name
+    || check.lastName === param.name || check.id === param.id)) {
+    return oneEmployees(param);
+  }
+  throw new Error('Informações inválidas');
+}
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage({ name: 'Wishart' }));
+// console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getEmployeesCoverage());
+// console.log(getEmployeesCoverage({ id: 'Id inválido' }));
 module.exports = getEmployeesCoverage;
